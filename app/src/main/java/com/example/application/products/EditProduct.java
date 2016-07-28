f42e53c3d5ef1243b6ac23af4e2840643b734120
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2016-07-28 14:44:23 +0900

    AppConpatActivity => Activityの変更などにより不必要なインポートを削除

--- app/src/main/java/com/example/application/products/EditProduct.java
+++ app/src/main/java/com/example/application/products/EditProduct.java
@@ -1,17 +1,19 @@
 package com.example.application.products;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-public class EditProduct extends AppCompatActivity implements View.OnClickListener{
+public class EditProduct extends Activity implements View.OnClickListener{
 
     private String mode = "";
     private int _id;
